99872740a7a56544d8391a815959f94a1b2722bc
Kelvin Ricafort <kelvs.r@outlook.com>
2024-06-03 14:10:06 +0800

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Kelvin on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
