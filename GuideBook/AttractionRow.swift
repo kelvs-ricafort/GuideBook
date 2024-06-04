e021799737953d4e6871128a1f5ad08981c59372
Kelvin Ricafort <kelvs.r@outlook.com>
2024-06-04 16:03:28 +0800

    - Building the City Card - Building the Attraction View - Building the Detail View

---
+++ GuideBook/AttractionRow.swift
@@ -0,0 +1,18 @@
+//
+//  AttractionRow.swift
+//  GuideBook
+//
+//  Created by Kelvin on 6/4/24.
+//
+
+import SwiftUI
+
+struct AttractionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttractionRow()
+}
---
+++ GuideBook/CityCard.swift
@@ -0,0 +1,18 @@
+//
+//  CityCard.swift
+//  GuideBook
+//
+//  Created by Kelvin on 6/4/24.
+//
+
+import SwiftUI
+
+struct CityCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CityCard()
+}
